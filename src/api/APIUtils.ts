98e1882b1d375c5c7ad0dcd70c9a57857611db19
nicbritos <nicbritos@hotmail.com>
2020-10-30 00:16:02 -0300

    Added API throttling

--- src/api/APIUtils.ts
+++ src/api/APIUtils.ts
@@ -2,9 +2,16 @@ import axios, {AxiosResponse} from 'axios';
 
 export abstract class APIUtils {
     static URL: string;
+    static previousResult: AxiosResponse | null = null;
 
-    static get<T>(): Promise<AxiosResponse<T>> {
-        return axios.get(APIUtils.URL);
+    static async get<T>(): Promise<AxiosResponse<T>> {
+        if (this.previousResult) return this.previousResult;
+
+        this.previousResult = await axios.get(APIUtils.URL);
+        setTimeout(args => {
+            APIUtils.previousResult = null;
+        }, 5000);
+        return this.previousResult!;
     }
 
     static post<T>(payload: Record<string, unknown>): Promise<AxiosResponse<T>> {
